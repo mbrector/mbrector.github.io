b564d2ba8748ac6459fb0b6ab9578d5083f1a896
mbrector <97118566+mbrector@users.noreply.github.com>
2022-01-04 12:07:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/kubotakx040.jpg') {
+    myImage.setAttribute ('src','images/cat314.jpg');
+  } else {
+    myImage.setAttribute ('src','images/kubotakx040.jpg');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Thanks for stopping by, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Thanks for stopping by, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
